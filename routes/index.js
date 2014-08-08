e660ce2e6e1d3b7e3d3441365fa33165e802d5ed
Otto Remse <otto.remse@gmail.com>
2014-08-08 14:35:14 +0200

    fixing upp tournamentdetails

--- routes/index.js
+++ routes/index.js
@@ -12,3 +12,6 @@ exports.tournaments = function (req, res) {
 exports.tournamentsdetails = function (req, res) {
     res.render('tournamentdetails.html');
 };
+exports.createnewtournament = function(req,res){
+  res.render('createnewtournament.html');
+};
