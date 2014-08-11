2aa8a5119b9e720e72e89566549f95d69b6dd4d4
grayoverride <johan.b.damm@gmail.com>
2014-08-11 05:04:43 +0200

    whoops

--- public/javascripts/app/services.js
+++ public/javascripts/app/services.js
@@ -3,10 +3,9 @@
 nTournamentServices.service('Tournament',['$resource',
 function ($resource) {
   var tournamentlist =[];
-  var tournamentpost = tournamentlist;
   var resourceData = $resource('/api/tourData');
   tournamentlist = resourceData.query();
-  tournamentpost = resourceData.save();
+
 this.tournaments = function () {
     return tournamentlist;
 };
@@ -14,8 +13,8 @@ this.tournament = function (tournamentId) {
     return Enumerable.From(tournamentlist).SingleOrDefault(null,function(i) { return i.tournamentId == tournamentId; });
 }
 this.addTournament = function (tournament) {
-    tournamentpost.push(tournament);
-    console.log(tournamentpost);
+    tournamentlist.push(tournament);
+    console.log(tournamentlist);
 
 }
 }
