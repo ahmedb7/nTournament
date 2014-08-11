8be2ef1826ffc8ceaf580eacd6c3a6e58c9a45cd
ottoremse <otto.remse@gmail.com>
2014-08-11 09:35:36 +0200

    moved app-scripts up one level and added folder

---
+++ public/app/services.js
@@ -0,0 +1,34 @@
+﻿var nTournamentServices = angular.module('nTournamentServices', ['ngResource']);
+
+nTournamentServices.service('Tournament',['$resource',
+function ($resource) {
+  var tournamentlist =[];
+  var resourceData = $resource('/api/tourData');
+  tournamentlist = resourceData.query();
+
+this.tournaments = function () {
+    return tournamentlist;
+};
+this.tournament = function (tournamentId) {
+    return Enumerable.From(tournamentlist).SingleOrDefault(null,function(i) { return i.tournamentId == tournamentId; });
+}
+this.addTournament = function (tournament) {
+    tournamentlist.push(tournament);
+
+}
+}
+]);
+nTournamentServices.service('News',['$resource',
+function ($resource){
+  var newslist = [];
+  var newsData = $resource('/data/news.json');
+  newslist = newsData.query();
+
+  this.news = function () {
+    return newslist;
+  }
+  this.addnewsdata = function (news) {
+      newslist.push(news);
+  }
+}
+]);
